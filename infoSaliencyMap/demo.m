c6889278bcc935bfbde9e3ff6c04df814ef75f79
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-04-28 21:21:41 +0000

    + Add resFld for path + name of the result folder

--- infoSaliencyMap/demo.m
+++ infoSaliencyMap/demo.m
@@ -5,7 +5,10 @@ function demo()
 % All images are initally reduced to a quater of its original size
 % There are 4 consecutive images used to calculate the temporal saliency
 % map.
-
+    %% Sample results
+    resFld = '/home/lengoanhcat/PhD_Research/trunk/Simulations/Results/Experiment_8/results_nc5/';
+    mkdir(resFld);
+    
     %% Sample images for testing
     imgPath1 = './figures/L_1.jpg'; % Image at t = -3;
     imgPath2 = './figures/L_2.jpg'; % Image at t = -2;
@@ -25,17 +28,17 @@ function demo()
     % Represent temporal saliency map
     figure(1), colormap('gray'), imagesc(tsm);
     title('Temporal Saliency Map - 2D');
-    saveas(1,'./results/tms-2D.jpg');
+    saveas(1,[resFld 'tms-2D.jpg']);
 
     % Represent spatial saliency map
     figure(2), colormap('gray'), imagesc(ssm);
     title('Spatial Saliency Map - 2D');
-    saveas(2,'./results/ssm-2D.jpg');    
+    saveas(2,[resFld 'ssm-2D.jpg']);    
     
     % Represent the saliency map
     figure(3), colormap('gray'), imagesc(ism);
     title('Information Saliency Map - 2D');
-    saveas(3,'./results/ims-2d.jpg');    
+    saveas(3,[resFld 'ims-2d.jpg']);    
     
     figure(4);    
     gridx1 = 1:1:size(img1,1);
@@ -43,5 +46,5 @@ function demo()
     [gridx2,gridx1] = meshgrid(gridx2,gridx1);
     surf(gridx1,gridx2,ism);   
     title('Information Saliency Map - 3D');
-    saveas(4,'./results/ims-3d.jpg');
+    saveas(4,[resFld 'ims-3d.jpg']);
 end
