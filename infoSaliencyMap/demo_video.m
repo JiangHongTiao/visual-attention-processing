286831a766211b3f578d11dff743912e9ee7d985
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-04-30 05:34:31 +0000

    ! Change filename video_systemObject.m into infoSaliencyMap_video.m + Add demo_video script

---
+++ infoSaliencyMap/demo_video.m
@@ -0,0 +1,6 @@
+function demo_video()
+    %% Input Video 1 
+    inputVideo =  'D:\PhD Research\UNMC_Autovision_DB\Lane\videoL_001.mpg';
+    outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
+    infoSaliencyMap_video(inputVideo,outFld,'dct',20);
+end
