f3179af9289fd4588fbef0b408af78f0a496cc89
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-05-03 11:52:18 +0000

    + Change the inputVideo folder

--- trunk/infoSaliencyMap/demo_video.m
+++ trunk/infoSaliencyMap/demo_video.m
@@ -1,6 +1,10 @@
 function demo_video()
     %% Input Video 1 
-    inputVideo =  'D:\PhD Research\UNMC_Autovision_DB\Lane\videoL_001.mpg';
+%     inputVideo =  'D:\PhD Research\UNMC_Autovision_DB\Lane\videoL_001.mpg';
+%     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
+%     infoSaliencyMap_video(inputVideo,outFld,'dct',20);
+    %% Input Video 2
+    inputVideo =  'D:\PhD Research\Simulations\Experiments\Experiment_8\Database\1.5.mp4';
     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
-    infoSaliencyMap_video(inputVideo,outFld,'dct',20);
+    infoSaliencyMap_video(inputVideo,outputFolder,'hadamard',20);
 end
