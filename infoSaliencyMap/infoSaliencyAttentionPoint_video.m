34da7f7ec071c3b7d83fefbafc152b41bbe8de79
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-05-07 09:27:58 +0000

    + basically finish infoSaliencyMap/infoSaliencyAttentionPoint_video.m function

--- infoSaliencyMap/infoSaliencyAttentionPoint_video.m
+++ infoSaliencyMap/infoSaliencyAttentionPoint_video.m
@@ -1,4 +1,4 @@
-function infoSaliencyMap_video(inVid,outFld,transEng,noCoff)
+function saliencyScore_ts = infoSaliencyAttentionPoint_video(inVid,transEng,noCoff,Loc)
 %% Developing Lane-mark Extraction by using 
 % This video_systemObject is created to test the idea of using "Information
 % Saliency Map" on video
@@ -8,12 +8,6 @@ function infoSaliencyMap_video(inVid,outFld,transEng,noCoff)
 % noCoff: number of reserved coefficients
 % Copyright 2010, The University of Nottingham
 
-
-%% Initialization
-% Use these next sections of code to initialize the required variables and
-% System objects.
-
-% inputVideo = 'D:\PhD Research\UNMC_Autovision_DB\Lane\videoL_001.mpg';
 %% 
 % Create a System object to read video from a video.
 hbfr = video.MultimediaFileReader( ...
@@ -22,103 +16,44 @@ hbfr = video.MultimediaFileReader( ...
         ,'VideoOutputPort',1 ...
         ,'ImageColorSpace','RGB' ...
         );
-    
-%% 
-% Create a System object to save video to file
-[path,name,ext] = fileparts(inVid);
-outFld = [outFld '\' name '_' transEng '_nc' num2str(noCoff)];
 
-if (exist(outFld,'dir') ~= 7) 
-    mkdir(outFld);
-else
-    warning('The folder is already existed');
-end
-
-hmfw1 = video.MultimediaFileWriter( ...
-        'Filename',[outFld '\video_sample.avi'] ...
-        ,'FileFormat','AVI' ...
-        ,'AudioInputPort',false ...
-        ,'VideoInputPort',true ...
-        ,'VideoCompressor','MJPEG Compressor');
-hmfw2 = video.MultimediaFileWriter( ...
-    'Filename',[outFld '\video_tsm.avi'] ...
-    ,'FileFormat','AVI' ...
-    ,'AudioInputPort',false ...
-    ,'VideoInputPort',true ...
-    ,'VideoCompressor','MJPEG Compressor');
-hmfw3 = video.MultimediaFileWriter( ...
-    'Filename',[outFld '\video_ssm.avi'] ...
-    ,'FileFormat','AVI' ...
-    ,'AudioInputPort',false ...
-    ,'VideoInputPort',true ...
-    ,'VideoCompressor','MJPEG Compressor');
-hmfw4 = video.MultimediaFileWriter( ...
-    'Filename',[outFld '\video_ism.avi'] ...
-    ,'FileFormat','AVI' ...
-    ,'AudioInputPort',false ...
-    ,'VideoInputPort',true ...
-    ,'VideoCompressor','MJPEG Compressor');
 %%
 % Create color space converter System objects to convert the image from
 % YCbCr to RGB format and from RGB to intensity format.
 hcsc1 = video.ColorSpaceConverter('Conversion', 'RGB to intensity');
 
-%%
-% Create a morphological erosion System object to thin out portions of the
-% road and objects other than cars which are also produced as a result of
-% segmentation.
-herode = video.MorphologicalErode('Neighborhood', strel('square',3));
-
-%%
-% Create an alphablend system object to represent the output of lane-mark
-% extraction
-halphablend = video.AlphaBlender('Operation','Blend','Opacity',0.25);
-
-% Predefined configuration variable
-% transEng = 'hadamard';% What type of transform engine: hadamard,dct
-% noCoff = 20; % Number of reserved components  
+%% 
+% Create the marker 
+hmi = video.MarkerInserter('Shape','X-mark','Size',10, ...                            
+                            'BorderColor','Black', ...
+                            'Opacity',1);
 
 % Initialize some variables used in plotting motion vectors.
 M = 5;
 hbfi = info(hbfr);
 iFrame = 0;
-frame_scale_ratio = 0.25;
+frame_scale_ratio = 1;
 frame_size = fliplr(hbfi.VideoSize)*frame_scale_ratio;
+
+saliencyScore_ts = timeseries;
+saliencyScore_ts.Name = 'Saliency Score in Attentive Points';
+saliencyScore_ts.TimeInfo.Units = 'seconds';
+saliencyScore_ts.TimeInfo.Increment = 0.04;
+saliencyScore_ts.TimeInfo.Start = 0;
+% saliencyScore_ts.TimeInfo.Format = 'dd-mmm-yyyy HH:MM:SS';
+% saliencyScore_ts.TimeInfo.Stardate = datestr(clock);
+% saliencyScore_ts.TreatNaNasMissing = false; 
 
 %% Put M-1 frames into FIFO sequences in the order from 2 to M
 queue = zeros([frame_size M]);
-% for iImg = 2:1:M            
-% %     aFrame = step(hbfr);
-% %     aFrame = step(hcsc1,aFrame);
-% %     aFrame = imresize(aFrame,frame_scale_ratio,'bilinear');    
-% %     queue(:,:,iImg) = aFrame;
-%     ~isDone(hbfr);
-%     queue(:,:,iImg) = imresize(step(hcsc1,step(hbfr)),frame_scale_ratio,'bilinear');
-% end
 
 %%
 % Create System objects to display the original video, motion vector video,
 % the thresholded video and the results.
-hvideo1 = video.VideoPlayer('WindowCaption', 'Original Video');
+hvideo1 = video.VideoPlayer('WindowCaption', 'Video Tracked');
 hvideo1.WindowPosition(1) = round(0.5*hvideo1.WindowPosition(1)) ;
 hvideo1.WindowPosition(2) = round(0.5*(hvideo1.WindowPosition(2))) ;
 hvideo1.WindowPosition([4 3]) = frame_size;
-
-hvideo2 = video.VideoPlayer('WindowCaption', 'Temporal Saliency Map');
-hvideo2.WindowPosition(1) = hvideo1.WindowPosition(1) + 350;
-hvideo2.WindowPosition(2) =round(1.5* hvideo2.WindowPosition(2));
-hvideo2.WindowPosition([4 3]) = frame_size;
-
-hvideo3 = video.VideoPlayer('WindowCaption', 'Spatial Saliency Map');
-hvideo3.WindowPosition(1) = hvideo2.WindowPosition(1) + 350;
-hvideo3.WindowPosition(2) = round(1.5*(hvideo3.WindowPosition(2))) ;
-hvideo3.WindowPosition([4 3]) = frame_size;
-
-hvideo4 = video.VideoPlayer('WindowCaption', 'Information Saliency Map');
-hvideo4.WindowPosition(1) = hvideo1.WindowPosition(1);
-hvideo4.WindowPosition(2) = round(0.3*(hvideo4.WindowPosition(2))) ;
-hvideo4.WindowPosition([4 3]) = frame_size;
-
 
 %% Stream processing loop
 % Create the processing loop to track objects in the input video. This
@@ -130,36 +65,27 @@ hvideo4.WindowPosition([4 3]) = frame_size;
 %% Process stream of videos
 while ~isDone(hbfr)    
     queue = circshift(queue,[0 0 -1]); % Rotate the queue to get new input value
-    queue(:,:,M) = imresize(step(hcsc1,step(hbfr)),frame_scale_ratio,'bilinear');        % Convert color image to intensity and scale it 1/4
+    queue(:,:,M) = step(hcsc1,step(hbfr));        % Convert color image to intensity and scale it 1/4
     iFrame = iFrame + 1;    
     if ( sum(sum(queue(:,:,1))) ~= 0 ) 
         % Detecting lane-mark by saliency method
-        tic;
-        [tsm,ssm,ism] = infoSaliencyMap(queue,transEng,noCoff);       
-        toc;
+        aSaliencyScore = infoSaliencyAttentionPoint(queue,transEng,noCoff,Loc.Data(:,:,iFrame));       
+        saliencyScore_ts = addsample(saliencyScore_ts,'Data',aSaliencyScore,'Time', (iFrame-1)*0.04);
+                
         %% Result Presentation in Grayscale or Color
-        step(hvideo1,queue(:,:,M));    
-        step(hvideo2,tsm);    
-        step(hvideo3,ssm);    
-        step(hvideo4,ism);    
-        step(hmfw1,queue(:,:,M));
-        step(hmfw2,tsm);
-        step(hmfw3,ssm);
-        step(hmfw4,ism);
+        tmpImg = step(hmi,queue(:,:,M),Loc.Data(:,:,iFrame));
+        step(hvideo1,tmpImg);         
     end
-%     if (iFrame >= 90) 
-%         break; 
-%     end    
+    if (iFrame >= 90) 
+        break; 
+    end    
 end
 
 %% Close
 % Here you call the close method on the System objects to close any open
 % files and devices.
 close(hbfr);
-close(hmfw1);
-close(hmfw2);
-close(hmfw3);
-close(hmfw4);
+
 %% Summary
 % The output video shows the cars which were tracked by drawing boxes
 % around them. The video also displays the number of cars being tracked.
