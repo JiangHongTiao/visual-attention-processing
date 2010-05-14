db66831483befa31d8bffebdb7f226526e63b2dd
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-05-14 03:30:52 +0000

    ! Change exp video

--- infoSaliencyMap/demo_video.m
+++ infoSaliencyMap/demo_video.m
@@ -4,11 +4,11 @@ function demo_video()
 %     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
 %     infoSaliencyMap_video(inputVideo,outFld,'dct',20);
     %% Input Video 2
-%     inputVideo =  'D:\PhD Research\Simulations\Experiments\Experiment_8\Database\1.5.mp4';
-%     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
-%     infoSaliencyMap_video(inputVideo,outputFolder,'hadamard',20);
-    %% Input Video 3
-    inputVideo =  'D:\PhD Research\UNMC_Autovision_DB\Lane\videoL_030.mpg';
+    inputVideo =  'D:\PhD Research\Simulations\Experiments\Experiment_8\Database\1.5.mp4';
     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
     infoSaliencyMap_video(inputVideo,outputFolder,'hadamard',30);
+    %% Input Video 3
+%     inputVideo =  'D:\PhD Research\UNMC_Autovision_DB\Lane\videoL_030.mpg';
+%     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
+%     infoSaliencyMap_video(inputVideo,outputFolder,'hadamard',30);
 end
