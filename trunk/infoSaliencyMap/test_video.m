87f711cee4e53758ca987585219e997262c41ad9
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-05-19 02:48:06 +0000

    ! Add video recording function into test_video.m

--- trunk/infoSaliencyMap/test_video.m
+++ trunk/infoSaliencyMap/test_video.m
@@ -1,4 +1,4 @@
-function iFrame = test_video(inVid)
+function iFrame = test_video(inVid,outFld)
 
 %% 
 % Create a System object to read video from a video.
@@ -19,12 +19,22 @@ hvideo1 = video.VideoPlayer('WindowCaption', 'Video Tracked');
 hvideo1.WindowPosition(1) = round(0.5*hvideo1.WindowPosition(1)) ;
 hvideo1.WindowPosition(2) = round(0.5*(hvideo1.WindowPosition(2))) ;
 hvideo1.WindowPosition([4 3]) = frame_size;    
-    
+
+%% 
+% Create a System object to save video to file
+hmfw1 = video.MultimediaFileWriter( ...
+        'Filename',[outFld '\test_sample.avi'] ...
+        ,'FileFormat','AVI' ...
+        ,'AudioInputPort',false ...
+        ,'VideoInputPort',true ...
+        ,'VideoCompressor','MJPEG Compressor');
+
 iFrame = 0;    
     %% Process stream of videos
     while ~isDone(hbfr)   
         img = step(hbfr);
         iFrame = iFrame + 1;
         step(hvideo1,img);
+        step(hmfw1,img);
     end
 end
