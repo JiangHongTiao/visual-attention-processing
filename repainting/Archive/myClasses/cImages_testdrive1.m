7d64fc3336d335efbbf755e7692214bef704ddc5
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-05-21 09:41:19 +0000

    - Remove unnecessary Archive

--- repainting/Archive/myClasses/cImages_testdrive1.m
+++
@@ -1,5 +0,0 @@
-function imgDB = cImages_testdrive1()
-    clear classes; fclose all; close all force; diary off;
-    addpath('/home/lengoanhcat/PhD_Research/trunk/Simulations/Experiments/Experiment_4/Archives/myClasses/');
-    imgDB = cImages('/home/lengoanhcat/Downloads/photos.db','yellow');
-end
