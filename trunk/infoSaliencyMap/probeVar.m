8899ab169c96638422cd3b0f7ec3ac1a9a8eb187
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-05-22 03:37:09 +0000

    + Modify probeVar to add save function

--- trunk/infoSaliencyMap/probeVar.m
+++ trunk/infoSaliencyMap/probeVar.m
@@ -1,19 +1,29 @@
-function ts_var = probeVar(variable,value)
+function probeVar(variable,mode,varargin)
 %%PROBEDATA The function is used to collect data and save those data into
 %% filename.mat
 
 eval(['persistent ' variable]);    
-eval(['ts_var = ' variable ';']);
-if isempty(ts_var)
-    ts_var = timeseries(variable);       
-    ts_var.TimeInfo.Units = 'seconds';
-    ts_var.TimeInfo.Increment = 0.04;
-    ts_var.TimeInfo.Start = 0;    
+
+if isequal(mode,'add') && (nargin == 3)
+    value = varargin{1};
+    eval(['ts_var = ' variable ';']);
+    warning('OFF');
+    if isempty(ts_var)        
+        ts_var = timeseries(variable);       
+        ts_var.TimeInfo.Units = 'seconds';
+        ts_var.TimeInfo.Increment = 0.04;
+        ts_var.TimeInfo.Start = 0;    
+        eval([variable ' = ts_var;']);
+    %     save(file,filename);  
+    end
+
+    ts_var = addsample(ts_var,'Data',value,'Time',numel(ts_var.Time)*0.04);
     eval([variable ' = ts_var;']);
-%     save(file,filename);  
+    warning('ON');
+elseif isequal(mode,'save') && (nargin == 2)
+    save(['./results/' variable],variable);
+else
+    warning('Invalid input');
 end
-
-ts_var = addsample(ts_var,'Data',value,'Time',numel(ts_var.Time)*0.04);
-eval([variable ' = ts_var;']);
-
+    
 end
