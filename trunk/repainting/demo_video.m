5c641daabdfc0c6c39a15d042ecc8e200427673e
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-05-25 03:48:01 +0000

    ! Change output file name

--- trunk/repainting/demo_video.m
+++ trunk/repainting/demo_video.m
@@ -1,13 +1,13 @@
 function demo_video();
     %% Input Video 1
-    inputVideo =  './results/1.5.mp4';
+    inputVideo =  './data/1.5.mp4';
     outputFolder = './results';
     load('./data/psychology_data.mat');    
     
     %%    
     % Create a System object to save data to file
     [~,name,~] = fileparts(inputVideo);
-    fileName = [name '_repainted.avi'];    
+    fileName = [name '_repainted_' datestr(now,'yyyymmddTHHMMSS') '.avi'];    
     outputVideo = [outputFolder '/' fileName];    
     repaint_video(inputVideo,outputVideo,Loc);            
 end
