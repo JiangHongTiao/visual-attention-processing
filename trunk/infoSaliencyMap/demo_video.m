3416acee95410dacff42e06d3fad3bad64547d4a
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-06-01 03:41:49 +0000

    ! Modify demo_video.m to make it more understandable

--- trunk/infoSaliencyMap/demo_video.m
+++ trunk/infoSaliencyMap/demo_video.m
@@ -4,9 +4,16 @@ function demo_video()
 %     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
 %     infoSaliencyMap_video(inputVideo,outFld,'dct',20);
     %% Input Video 2
+    
+    %% 
+    % Create a System object to save video to file
     inputVideo =  './data/1.5.mp4';
-    outputFolder = '.\results';
-    infoSaliencyMap_video(inputVideo,outputFolder,'hadamard',30);
+    outputFolder = './results';
+    transformEngine = 'hadamard';
+    numberOfCoefficients = 30;
+    [~,name,~] = fileparts(inputVideo);
+    outputFolder = [outputFolder '\' name '_engine-' transformEngine '_nc-' num2str(numberOfCoefficients) '_date-' datestr(now,'yyyymmddTHHMMSS')];    
+    infoSaliencyMap_video(inputVideo,outputFolder,transformEngine,numberOfCoefficients);
     %% Input Video 3
 %     inputVideo =  'D:\PhD Research\UNMC_Autovision_DB\Lane\videoL_030.mpg';
 %     outputFolder = 'D:\PhD Research\Simulations\Results\Experiment_8';
