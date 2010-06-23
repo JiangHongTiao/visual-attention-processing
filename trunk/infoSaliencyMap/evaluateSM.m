6bb897615feb3ebe86bc5270e8c3e91742a9ea8b
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-06-23 06:47:12 +0000

    ! remove evaluate2 function

--- trunk/infoSaliencyMap/evaluateSM.m
+++ trunk/infoSaliencyMap/evaluateSM.m
@@ -1,4 +1,4 @@
-function evaluateSM()
+function evaluateSM(binFileName)
 %evaluateSM a set of methods for comparing between saliency map and human
 % eyes-fixed points.
 % Method 1: drawing the graph for saliency at fixated points during the
@@ -88,66 +88,6 @@ function evaluation1(binFileName,noFrames)
          saveas(1,'normalized_saliency_score.fig');             
          saveas(2,'histogram_saliency_score.fig');
          save('evalData1.mat','evalData1');
-         cd(currentFolder);
-         close all;
-     end
-end
-
-function evaluation2(binFileName,noFrames,noFramesEachPlot) 
-    %% Reading the binary video saliency maps 
-    hbfr = video.BinaryFileReader( ...
-            'Filename',binFileName, ...
-            'VideoFormat','Custom', ...
-            'BitstreamFormat','Planar', ...
-            'VideoComponentCount',1,...
-            'SignedData',true,...
-            'VideoComponentSizes',[135 180], ...
-            'VideoComponentBits',[16]);    
-        
-    %% Loading the location of eye-fixated points
-    load('./data/1.5_LocData_full_modification_1.mat');
-    
-     %% Initialization
-     noPlots = ceil(noFrames / noFramesEachPlot)
-     
-     for iPlot = 1:1:noPlots
-        iFrame = 0;
-        sSaliency = zeros(3,noFramesEachPlot);      
-        while ~isDone(hbfr)
-            iFrame = iFrame + 1;
-            img = step(hbfr);
-            cEyeFixatedPoint = Loc.Data(:,:,(iPlot-1)*noFramesEachPlot + iFrame);
-            y = round(cEyeFixatedPoint(1)/4);
-            x = round(cEyeFixatedPoint(2)/4);
-            sSaliency(1,iFrame) = min(min(img));
-            sSaliency(2,iFrame) = img(y,x);
-            sSaliency(3,iFrame) = max(max(img));            
-            if (iFrame >= noFramesEachPlot)
-                break;
-            end
-        end
-
-        figure(iPlot);
-        plot(sSaliency(1,:),'--rs');
-        hold all;
-        plot(sSaliency(2,:),'--bo');
-        hold all;
-        plot(sSaliency(3,:),'--g+');
-        legend('Minimum Saliency Score','Eye-fixated Point Saliency Score','Maximum Saliency Score');
-        title(['Saliency score at driver-eye fixated points ( Frame ' num2str((iPlot-1)*noFramesEachPlot) ' -> ' 'Frame ' num2str(iPlot*noFramesEachPlot) ' )']);
-        xlabel('Order of frames');
-        ylabel('Saliency Score');         
-     end
-     
-     savePlotFlag = 1;
-     if (savePlotFlag == 1)
-         outputFolder = ['./results/evaluation2_graphs_&_data_date-' datestr(now,'yyyymmddTHHMMSS')];
-         mkdir(outputFolder);
-         currentFolder = pwd;
-         cd(outputFolder);
-         for iPlot = 1:1:noPlots
-             saveas(iPlot,[num2str(iPlot) '.fig']);             
-         end
          cd(currentFolder);
          close all;
      end
