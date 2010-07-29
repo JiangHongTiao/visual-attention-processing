c6485542433a8ff80a063f545e9e057467bad53b
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-07-29 01:13:40 +0000

    ! Test number of frames 2

--- branches/infoSaliencyMap_0.2/demo.m
+++ branches/infoSaliencyMap_0.2/demo.m
@@ -14,7 +14,7 @@ function demo()
     %% Predefined parameters 
     transEng = 'hadamard';% What type of transform engine: hadamard,dct
     noCoff = 30; % Number of reserved components        
-    noImgs = 6;
+    noImgs = 2;
     szPatches = 8;
     %% Sample images for testing with M = 4
 %     imgPath1 = './figures/set4/L_1.jpg'; % Image at t = -3;
@@ -32,25 +32,25 @@ function demo()
 %     imgs = cat(3,img1,img2,img3,img4,img5);          
 
     %% Sample images for testing with M = 8
-%     imgPath1 = './figures/set8/L_1.jpg'; 
-%     imgPath2 = './figures/set8/L_2.jpg'; 
-%     imgPath3 = './figures/set8/L_3.jpg'; 
-%     imgPath4 = './figures/set8/L_4.jpg'; 
-%     imgPath5 = './figures/set8/L_5.jpg'; 
-%     imgPath6 = './figures/set8/L_6.jpg'; 
-%     imgPath7 = './figures/set8/L_7.jpg'; 
-%     imgPath8 = './figures/set8/L_8.jpg'; 
-%     imgPath9 = './figures/set8/L_9.jpg'; 
+    imgPath1 = './figures/set8/L_1.jpg'; 
+    imgPath2 = './figures/set8/L_2.jpg'; 
+    imgPath3 = './figures/set8/L_3.jpg'; 
+    imgPath4 = './figures/set8/L_4.jpg'; 
+    imgPath5 = './figures/set8/L_5.jpg'; 
+    imgPath6 = './figures/set8/L_6.jpg'; 
+    imgPath7 = './figures/set8/L_7.jpg'; 
+    imgPath8 = './figures/set8/L_8.jpg'; 
+    imgPath9 = './figures/set8/L_9.jpg'; 
     
-    imgPath1 = './figures/set8_1/frame-0001.jpg'; 
-    imgPath2 = './figures/set8_1/frame-0002.jpg'; 
-    imgPath3 = './figures/set8_1/frame-0003.jpg'; 
-    imgPath4 = './figures/set8_1/frame-0004.jpg'; 
-    imgPath5 = './figures/set8_1/frame-0005.jpg'; 
-    imgPath6 = './figures/set8_1/frame-0006.jpg'; 
-    imgPath7 = './figures/set8_1/frame-0007.jpg'; 
-    imgPath8 = './figures/set8_1/frame-0008.jpg'; 
-    imgPath9 = './figures/set8_1/frame-0009.jpg'; 
+%     imgPath1 = './figures/set8_1/frame-0001.jpg'; 
+%     imgPath2 = './figures/set8_1/frame-0002.jpg'; 
+%     imgPath3 = './figures/set8_1/frame-0003.jpg'; 
+%     imgPath4 = './figures/set8_1/frame-0004.jpg'; 
+%     imgPath5 = './figures/set8_1/frame-0005.jpg'; 
+%     imgPath6 = './figures/set8_1/frame-0006.jpg'; 
+%     imgPath7 = './figures/set8_1/frame-0007.jpg'; 
+%     imgPath8 = './figures/set8_1/frame-0008.jpg'; 
+%     imgPath9 = './figures/set8_1/frame-0009.jpg'; 
     
     % All images are resized by 1/2
     scaleValue = 1;
