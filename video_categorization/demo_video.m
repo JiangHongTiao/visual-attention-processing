a5a427a4fed71d3f9a602e2a8d380d0cb7d1265f
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-08-04 11:48:06 +0000

    + add demoFlag and pftSaliencyMap running flag into the demo_video.m

--- video_categorization/demo_video.m
+++ video_categorization/demo_video.m
@@ -5,26 +5,31 @@ function demo_video()
     % I/O Part
     dbname = 'digikam4.db';
     albname = 'autosplit';
-%     ptagname = 'our_car';
-%     ctagname = 'go_up';
-    inputVideos =  cVideos(dbname,albname);    
-    outputFolder = ['./results' '/' albname '/pftSaliencyMaps_date-' datestr(now,'yyyymmddTHHMMSS')];
-    saveFlag = 1;    
-    if (saveFlag == 1)
-        if (exist(outputFolder,'dir') ~= 7) 
-            mkdir(outputFolder);
-        else
-            warning('The folder is already existed');
-        end
-    end        
+    ptagname = 'our_car';
+    ctagname = 'go_up';
+    inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
+    saveFlag = 0; 
+    demoFlag = 1;
+    pftSaliencyMap = 1; pqftSaliencyMap = 0;
     
-    for i = 1:1:length(inputVideos.vidpaths)    
-        inputVideo = inputVideos.vidpaths{i};
-        [~,name,~] = fileparts(inputVideo);
-        
-        outputVideo = [outputFolder '/' name];
-        % saveFlag is used for controlling save action
-        % processing part    
-        pftSaliencyMap_video(inputVideo,outputVideo,saveFlag);
+    if (pftSaliencyMap == 1)
+        outputFolder = ['./results' '/' albname '/pftSaliencyMaps_date-' datestr(now,'yyyymmddTHHMMSS')];    
+        if (saveFlag == 1)
+            if (exist(outputFolder,'dir') ~= 7) 
+                mkdir(outputFolder);
+            else
+                warning('The folder is already existed');
+            end
+        end        
+
+        for i = 1:1:length(inputVideos.vidpaths)    
+            inputVideo = inputVideos.vidpaths{i};
+            [~,name,~] = fileparts(inputVideo);
+
+            outputVideo = [outputFolder '/' name];
+            % saveFlag is used for controlling save action
+            % processing part    
+            pftSaliencyMap_video(inputVideo,outputVideo,saveFlag,demoFlag);
+        end
     end
 end
