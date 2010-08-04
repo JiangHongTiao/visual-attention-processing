e2392e5adb5230ca20034ec3bb239cb2a52252cc
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-08-04 22:49:42 +0000

    ! Add pqft part into the demo_video.m code

--- trunk/video_categorization/demo_video.m
+++ trunk/video_categorization/demo_video.m
@@ -1,16 +1,17 @@
 function demo_video()
     % includepath
     addpath('../freqSaliencyMap/pft');
-    
+    addpath('../freqSaliencyMap/pqft');
     % I/O Part
     dbname = 'digikam4.db';
     albname = 'autosplit';
-    ptagname = 'our_car';
-    ctagname = 'go_up';
-    inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
-    saveFlag = 0; 
-    demoFlag = 1;
-    pftSaliencyMap = 1; pqftSaliencyMap = 0;
+%     ptagname = 'our_car';
+%     ctagname = 'go_up';
+%     inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
+    inputVideos =  cVideos(dbname,albname);
+    saveFlag = 1; 
+    demoFlag = 0;
+    pftSaliencyMap = 0; pqftSaliencyMap = 1;
     
     if (pftSaliencyMap == 1)
         outputFolder = ['./results' '/' albname '/pftSaliencyMaps_date-' datestr(now,'yyyymmddTHHMMSS')];    
@@ -30,6 +31,27 @@ function demo_video()
             % saveFlag is used for controlling save action
             % processing part    
             pftSaliencyMap_video(inputVideo,outputVideo,saveFlag,demoFlag);
+        end
+    end
+    
+    if (pqftSaliencyMap == 1)
+        outputFolder = ['./results' '/' albname '/pqftSaliencyMaps_date-' datestr(now,'yyyymmddTHHMMSS')];    
+        if (saveFlag == 1)
+            if (exist(outputFolder,'dir') ~= 7) 
+                mkdir(outputFolder);
+            else
+                warning('The folder is already existed');
+            end
+        end        
+
+        for i = 1:1:length(inputVideos.vidpaths)    
+            inputVideo = inputVideos.vidpaths{i};
+            [~,name,~] = fileparts(inputVideo);
+
+            outputVideo = [outputFolder '/' name];
+            % saveFlag is used for controlling save action
+            % processing part    
+            pqftSaliencyMap_video(inputVideo,outputVideo,saveFlag,demoFlag);
         end
     end
 end
