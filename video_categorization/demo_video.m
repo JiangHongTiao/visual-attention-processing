9b0616ef372b9af18012b363de1eb4137b7633f8
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-08-11 13:20:43 +0000

    ! add infoSaliencyMap_3.m part into video categorization

--- video_categorization/demo_video.m
+++ video_categorization/demo_video.m
@@ -4,17 +4,18 @@ function demo_video()
     addpath('../freqSaliencyMap/pqft');
     addpath('../ittiSaliencyMap/');
     addpath('../gbvsSaliencyMap/');
-    addpath('../infoSaliencyMap/');
+%     addpath('../infoSaliencyMap/');
+    addpath('../../vap_svn_ghost_branches/infoSaliencyMap_0.3/');
     % I/O Part
     dbname = 'digikam4.db';
     albname = 'autosplit';
-    ptagname = 'our_car';
-    ctagname = 'go_up';
-    inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
-%     inputVideos =  cVideos(dbname,albname);
-    saveFlag = 0; 
-    demoFlag = 1;
-    pftSaliencyMap = 0; pqftSaliencyMap = 0; ittiSaliencyMap = 0; gbvsSaliencyMap = 0; infoSaliencyMap = 1;
+%     ptagname = 'our_car';
+%     ctagname = 'go_up';
+%     inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
+    inputVideos =  cVideos(dbname,albname);
+    saveFlag = 1;
+    demoFlag = 0;
+    pftSaliencyMap = 0; pqftSaliencyMap = 0; ittiSaliencyMap = 0; gbvsSaliencyMap = 0; infoSaliencyMap = 0; infoSaliencyMap_3 = 1;
     
     if (pftSaliencyMap == 1)
         outputFolder = ['./results' '/' albname '/pftSaliencyMaps_date-' datestr(now,'yyyymmddTHHMMSS')];    
@@ -150,4 +151,31 @@ function demo_video()
             infoSaliencyMap_video_1(inputVideo,outputVideo,saveFlag,demoFlag,inputData);
         end
     end       
+    
+    if (infoSaliencyMap_3 == 1)
+        outputFolder = ['./results' '/' albname '/infoSaliencyMaps_3_date-' datestr(now,'yyyymmddTHHMMSS')];
+        if (saveFlag == 1)
+            if (exist(outputFolder,'dir') ~= 7) 
+                mkdir(outputFolder);
+            else
+                warning('The folder is already existed');
+            end
+        end        
+
+        for i = 1:1:length(inputVideos.vidpaths)    
+            inputVideo = inputVideos.vidpaths{i};    
+            
+            %% Add eye-fixated location data
+            inputData = inputVideo;
+            inputData(end-2:end) = 'mat';
+            load(inputData);
+            inputData = subLoc.Data;
+            
+            [~,name,~] = fileparts(inputVideo);            
+            outputVideo = [outputFolder '/' name];
+            % saveFlag is used for controlling save action
+            % processing part    
+            infoSaliencyMap_video_3(inputVideo,outputVideo,saveFlag,demoFlag,inputData);
+        end
+    end           
 end
