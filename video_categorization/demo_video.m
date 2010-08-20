8790c47d6eff02bad90dc22e53a1aead516c26d0
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-08-20 08:37:26 +0000

    ! run for infoSaliencyMap only

--- video_categorization/demo_video.m
+++ video_categorization/demo_video.m
@@ -4,18 +4,18 @@ function demo_video()
     addpath('../freqSaliencyMap/pqft');
     addpath('../ittiSaliencyMap/');
     addpath('../gbvsSaliencyMap/');
-%     addpath('../infoSaliencyMap/');
-    addpath('../../vap_svn_ghost_branches/infoSaliencyMap_0.3/');
+    addpath('../infoSaliencyMap/');
+%     addpath('../../vap_svn_ghost_branches/infoSaliencyMap_0.3/');
     % I/O Part
     dbname = 'digikam4.db';
     albname = 'autosplit';
-%     ptagname = 'our_car';
-%     ctagname = 'go_up';
-%     inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
-    inputVideos =  cVideos(dbname,albname);
-    saveFlag = 1;
-    demoFlag = 0;
-    pftSaliencyMap = 0; pqftSaliencyMap = 0; ittiSaliencyMap = 0; gbvsSaliencyMap = 0; infoSaliencyMap = 0; infoSaliencyMap_3 = 1;
+    ptagname = 'our_car';
+    ctagname = 'go_up';
+    inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
+%     inputVideos =  cVideos(dbname,albname);
+    saveFlag = 0;
+    demoFlag = 1;
+    pftSaliencyMap = 0; pqftSaliencyMap = 0; ittiSaliencyMap = 0; gbvsSaliencyMap = 0; infoSaliencyMap = 1; infoSaliencyMap_3 = 0;
     
     if (pftSaliencyMap == 1)
         outputFolder = ['./results' '/' albname '/pftSaliencyMaps_date-' datestr(now,'yyyymmddTHHMMSS')];    
