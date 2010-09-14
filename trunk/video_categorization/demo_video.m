da2aac0e68b0f6b89ea8a45f9e7dfff8f115bb4d
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-09-14 00:34:47 +0000

    ! change position of Flag initialization code

--- trunk/video_categorization/demo_video.m
+++ trunk/video_categorization/demo_video.m
@@ -4,18 +4,23 @@ function demo_video()
     addpath('../freqSaliencyMap/pqft');
     addpath('../ittiSaliencyMap/');
     addpath('../gbvsSaliencyMap/');
-    addpath('../infoSaliencyMap/');
-%     addpath('../../vap_svn_ghost_branches/infoSaliencyMap_0.3/');
+%     addpath('../infoSaliencyMap/');
+
+    % Flag
+    saveFlag = 1;
+    demoFlag = 0;
+
     % I/O Part
     dbname = 'digikam4.db';
     albname = 'autosplit';
-%     ptagname = 'our_car';
-%     ctagname = 'go_up';
-%     inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
-    inputVideos =  cVideos(dbname,albname);
-    saveFlag = 1;
-    demoFlag = 0;
-    pftSaliencyMap = 0; pqftSaliencyMap = 0; ittiSaliencyMap = 0; gbvsSaliencyMap = 0; infoSaliencyMap = 1; infoSaliencyMap_3 = 0;
+    if (demoFlag == 1)
+        ptagname = 'our_car';
+        ctagname = 'go_up';
+        inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
+    else
+        inputVideos =  cVideos(dbname,albname);
+    end
+    pftSaliencyMap = 0; pqftSaliencyMap = 0; ittiSaliencyMap = 0; gbvsSaliencyMap = 0; infoSaliencyMap = 0; infoSaliencyMap_3 = 1;
     
     if (pftSaliencyMap == 1)
         outputFolder = ['./results' '/' albname '/pftSaliencyMaps_date-' datestr(now,'yyyymmddTHHMMSS')];    
@@ -151,7 +156,8 @@ function demo_video()
             infoSaliencyMap_video_1(inputVideo,outputVideo,saveFlag,demoFlag,inputData);
         end
     end       
-    
+
+    addpath('../../vap_svn_ghost_branches/infoSaliencyMap_0.3/');    
     if (infoSaliencyMap_3 == 1)
         outputFolder = ['./results' '/' albname '/infoSaliencyMaps_3_date-' datestr(now,'yyyymmddTHHMMSS')];
         if (saveFlag == 1)
@@ -162,7 +168,7 @@ function demo_video()
             end
         end        
 
-        for i = 1:1:length(inputVideos.vidpaths)    
+        for i = 5:1:length(inputVideos.vidpaths)    
             inputVideo = inputVideos.vidpaths{i};    
             
             %% Add eye-fixated location data
