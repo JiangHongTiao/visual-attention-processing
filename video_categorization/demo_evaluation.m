fa384fd41184e47754b433a9ab9ba8c4a2dcdd96
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-09-14 00:33:55 +0000

    ! change position of Flag initialization code

--- video_categorization/demo_evaluation.m
+++ video_categorization/demo_evaluation.m
@@ -1,15 +1,20 @@
 function demo_evaluation()
+% Flag 
+saveFlag = 0;
+demoFlag = 1;
 
 % I/O Part
 dbname = 'digikam4.db';
 albname = 'autosplit';
-%     ptagname = 'our_car';
-%     ctagname = 'go_up';
-%     inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
-inputVideos =  cVideos(dbname,albname);
-saveFlag = 1;
-demoFlag = 1;
-ittiSaliencyMap = 1; gbvsSaliencyMap = 1; pftSaliencyMap = 1; pqftSaliencyMap = 1; infoSaliencyMap = 1; entroSaliencyMap = 1;
+if demoFlag == 1
+    ptagname = 'our_car';
+    ctagname = 'go_up';
+    inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
+else
+    inputVideos =  cVideos(dbname,albname);
+end
+
+ittiSaliencyMap = 0; gbvsSaliencyMap = 0; pftSaliencyMap = 0; pqftSaliencyMap = 0; infoSaliencyMap = 0; entroSaliencyMap = 1;
 
 params.saliencyMethods = [ittiSaliencyMap, gbvsSaliencyMap, pftSaliencyMap, pqftSaliencyMap, infoSaliencyMap, entroSaliencyMap];
 params.saliencyMethodsInfo = {'ittiSaliencyMap','gbvsSaliencyMap', 'pftSaliencyMap', 'pqftSaliencyMap', 'infoSaliencyMap', 'entroSaliencyMap'};
@@ -20,8 +25,8 @@ params1.savePath = ['./results' '/' albname '/saliencyValueByRandomModel_date-' 
 
 params2 = params;
 params2.savePath = ['./results' '/' albname '/chanceAdjustedSaliency_date-' datestr(now,'yyyymmddTHHMMSS')];
-params2.rmmPath = params1.savePath;
-
+% params2.rmmPath = params1.savePath;
+params2.rmmPath = './results/autosplit/saliencyValueByRandomModel_date-20100815T181918';
 params3 = params2;
 params3.casPath = params2.savePath;
 
@@ -46,19 +51,19 @@ for i = 1:1:length(inputVideos.vidpaths)
     % saveFlag is used for controlling save action
     % processing part    
 %% Extract the saliency value at eye marks    
-%     SVAEM_Extraction(inputVideo,1,0,params);
+%     SVAEM_Extraction(inputVideo,1,0,params); % Step 1
     
-%% Extract the saliency values at eye marks
-    SVBRM_Extraction(inputVideo,1,0,params1);    
-    CAS_Calculation(inputVideo,1,1,params2);
+%% Extract the CAS saliency values at eye marks
+%     SVBRM_Extraction(inputVideo,1,0,params1); % Step 2
+    CAS_Calculation(inputVideo,1,0,params2); % Step 2
     
 % %% Extract the maximum saliency values of a frame
 % %     SVAMP_Extraction(inputVideo,1,0,params4);
 % %     NSV_Calculation(inputVideo,1,0,params5);
 
 %% Threshold saliency map by 128 and count percentage 
-%     SPAVG_Extraction(inputVideo,1,0,params6);
+%     SPAVG_Extraction(inputVideo,1,0,params6); % Step 3
 end
-CAS_Display(inputVideos,1,1,params3);
-%  AVG_Mean(inputVideos,1,1,params7);
+CAS_Display(inputVideos,1,1,params3); % Step 2
+%  AVG_Mean(inputVideos,1,1,params7); % Step 3
 end
