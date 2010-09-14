87732962ab6a17572f85a561ad19fc609e8a1463
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-09-14 00:35:43 +0000

    ! merge saliencyValueAtEyeMarks data

--- video_categorization/mergeData.m
+++ video_categorization/mergeData.m
@@ -5,8 +5,8 @@ albname = 'autosplit';
 %     ctagname = 'go_up';
 %     inputVideos =  cVideos(dbname,albname,ptagname,ctagname);    
 inputVideos =  cVideos(dbname,albname);
-dataPath1 = './results/autosplit/chanceAdjustedSaliency_date-20100814T224139';
-dataPath2 = './results/autosplit/chanceAdjustedSaliency_date-20100829T212548';
+dataPath1 = './results/autosplit/saliencyValueAtEyeMarks_date-20100814T113054';
+dataPath2 = './results/autosplit/saliencyValueAtEyeMarks_date-20100912T083028';
 saveFlag = 1;
 demoFlag = 1;
 
@@ -17,16 +17,16 @@ for i = 1:1:length(inputVideos.vidpaths)
     % processing part
     data1 = load([dataPath1 '/' inputVideo '.mat']);
     data2 = load([dataPath2 '/' inputVideo '.mat']);
-    data1.CAS(:,5) = data2.CAS(:,5);
+    data1.salValAtEyeMarks(:,5) = data2.salValAtEyeMarks(:,5);
     if demoFlag == 1 
-        disp(data1.CAS);
+        disp(data1.salValAtEyeMarks);
         pause;
     end
     if saveFlag == 1
         currentFolder = pwd;
         cd(dataPath1);
-        CAS = data1.CAS;        
-        save([inputVideo '.mat'],'CAS');
+        salValAtEyeMarks = data1.salValAtEyeMarks;        
+        save([inputVideo '.mat'],'salValAtEyeMarks');
         cd(currentFolder);
     end
 end
