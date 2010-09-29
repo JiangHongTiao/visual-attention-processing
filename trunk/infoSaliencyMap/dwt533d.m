924f3bbf0e8e24b6ae7c267aa2dec355f58bf5ff
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-09-29 10:50:34 +0000

    ! change variables for number of layers, rows, columns

--- trunk/infoSaliencyMap/dwt533d.m
+++ trunk/infoSaliencyMap/dwt533d.m
@@ -1,14 +1,16 @@
 function output_ims = dwt533d(A)
     % have to do dct3 on three-dimensional 4x4x4 block
-    PatchSize = size(A,1);
+    numOfLayers = size(A,3);
+    numOfRows = size(A,1);
+    numOfCols = size(A,2);
     output_ims = zeros(size(A));
     % Do the 2D dct for each frame
-    for depth = 1:PatchSize
+    for depth = 1:numOfLayers
         [~,~,~,~,output_ims(:,:,depth)] = dwt532d(A(:,:,depth));
     end;
     % Now do the dct for each row-column
-    for row = 1:PatchSize
-        for col = 1:PatchSize
+    for row = 1:numOfRows
+        for col = 1:numOfCols
             [~,~,output_ims(row,col,:)] = dwt531d(output_ims(row,col,:));
         end;
     end;    
