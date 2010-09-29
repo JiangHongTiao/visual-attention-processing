be4ae9b9e3381314a408a72591817a79d9d6ed8e
lengoanhcat <lengoanhcat@344fe5f2-a0ff-11de-bc7b-8ff00adae793>
2010-09-29 11:33:07 +0000

    ! change BestBasis2d function

--- trunk/infoSaliencyMap/BestBasis2d.m
+++ trunk/infoSaliencyMap/BestBasis2d.m
@@ -5,10 +5,9 @@ function [ DwtScore ] = BestBasis3d( patch, numLevels)
 
 %% Cost Value Calculation function
 % Best Basis Function with input: patch, numLevels; and
-% output: DwtScore;
-[dwt_patch,CostValues] = CostValueCalculation(patch,numLevels); 
-[Basis,DwtScore] = BestBasisSearch(CostValues,numLevels);
-                    
+% output: DwtScore;                    
+[dwt_patch,CostValues] = CostValueCalculation(patch,numLevels);                             
+[Basis,DwtScore] = BestBasisSearch(CostValues,numLevels);                    
 end
 
 function [basis,score] = BestBasisSearch(costValues,numLevels)
@@ -16,12 +15,11 @@ function [basis,score] = BestBasisSearch(costValues,numLevels)
 % Input: costValues,numLevels
 % Output: basis, score
     %% Bottom-up search for the best basis
-    b = 8; % b base of dwt in 3d
-    basis = [zeros((b^(numLevels-1)-1)/(b-1),1);ones(b^(numLevels-1),1)];
+    basis = [zeros((4^(numLevels-1)-1)/3,1);ones(4^(numLevels-1),1)];
     for j = numLevels-1:-1:1
-        for k = (b^(j-1)-1)/(b-1)+1:1:(b^j-1)/(b-1)
+        for k = (4^(j-1)-1)/3+1:1:(4^j-1)/3
             v1 = costValues(k);
-            v2 = sum(costValues(k*b-6:k*b+1));
+            v2 = costValues(k*4-2) + costValues(k*4-1) + costValues(k*4) + costValues(k*4+1);
             if v1 <= v2
                 basis(k) = 1;
             else
@@ -31,9 +29,9 @@ function [basis,score] = BestBasisSearch(costValues,numLevels)
     end
 
     %% Fill with 2's below the chosen basis
-    for k = 1:1:((b^(numLevels-1)-1)/(b-1))
+    for k = 1:1:((4^(numLevels-1)-1)/3)
         if basis(k) == 1 || basis(k) == 2
-            basis(k*b-6:k*b+1) = 2;
+            basis(4*k-2) = 2; basis(4*k-1) = 2; basis(4*k) = 2; basis(4*k+1) = 2;
         end
     end
 
@@ -47,40 +45,37 @@ function [DwtPatch,CostValues] = CostValueCalculation(patch,numLevels,costFunc)
 % Input: numLevels: number of dividing levels
 %        patch: a PatchSizexPatchSize patch, i.e 4x4,8x8,16x16 patches.
 % Output: CostValues: cost value of patches with different levels
-    b = 8; % b base of dwt3d is 8
     PatchSize = size(patch,1);
     costValue_index = 0;
-    CostValues = zeros((b^numLevels-1)/(b-1),1);            
+    CostValues = zeros((4^numLevels-1)/3,1);            
     noPatchRows = size(patch,1);
     noPatchCols = size(patch,2);
-    noPatchLays = size(patch,3);
+    
     for level_index = 0:1:(numLevels-1)               
         %patch_size
         subPatchSize = PatchSize / 2^level_index;        
         for row_index = 1:subPatchSize:noPatchRows
             for col_index = 1:subPatchSize:noPatchCols
-                for lay_index = 1:subPatchSize:noPatchLays
-                    subPatch = patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1),lay_index:(lay_index+subPatchSize-1));
-                    subPatch = dwt533d(subPatch);
-                    patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1),lay_index:(lay_index+subPatchSize-1)) = subPatch;
-                    % calculate shannon entropy for dct patch
-                    a        = subPatch(:);
-                    if sum(a ~= 0) > 0
-                        a        = a ./ norm(a);
-                        DwtScore = -sum(a(find(a)).^2.*log(a(find(a)).^2));
-                        costValue_index = costValue_index + 1;
-                        CostValues(costValue_index) = DwtScore;
-                    else
-                        costValue_index = costValue_index + 1;
-                        CostValues(costValue_index) = 0;
-                    end
-                    
-%                     if (level_index < numLevels-1) 
-%     %                     subPatch = subPatch./norm(subPatch);                
-%     %                     [cA,cH,cV,cD] = dwt2(subPatch,'db1');   % have to do dwt2 on two-dimensional 4x4 block                        
-%                         patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1),lay_index:(lay_index+subPatchSize-1)) = dwt533d(subPatch);
-%                     end
-                end
+                subPatch = patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1));
+                [~,~,~,~,subPatch] = dwt532d(subPatch);
+                patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1)) = subPatch;
+                % calculate shannon entropy for dct patch
+                a        = subPatch(:);
+                if sum(a ~= 0) > 0
+                    a        = a ./ norm(a);
+                    DwtScore = -sum(a(find(a)).^2.*log(a(find(a)).^2));
+                    costValue_index = costValue_index + 1;
+                    CostValues(costValue_index) = DwtScore;
+                else
+                    costValue_index = costValue_index + 1;
+                    CostValues(costValue_index) = 0;
+                end                
+%                 if (level_index < numLevels-1) 
+% %                     subPatch = subPatch./norm(subPatch);                
+% %                     [cA,cH,cV,cD] = dwt2(subPatch,'db1');   % have to do dwt2 on two-dimensional 4x4 block
+%                     [cA,cH,cV,cD,~] = dwt532d(subPatch);
+%                     patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1)) = [cA,cV;cH,cD];                
+%                 end
             end
         end      
     end
--- trunk/infoSaliencyMap/KenSpatioTemporal3dBestBasis.m
+++ trunk/infoSaliencyMap/KenSpatioTemporal3dBestBasis.m
@@ -13,7 +13,7 @@ normSpaceFlg = 1;
 dctSpaceFlg = 1;
 dwtSpaceFlg = 1;
 % Save the results
-savFlg = 1;
+savFlg = 0;
 
 % Options definition
 repOpt = 2; % 1: thresholded; 2: top N points
@@ -21,7 +21,7 @@ threshold = 0.9;
 numOfTopPoints = 20;
 
 % Image Paths
-inFld = 'hall_objects_qcif';
+inFld = 'akiyo_cif';
 imgPath = ['../imageTestSequence/' inFld '/'];
 if repOpt == 1
     resFld = ['./results/KenSpatioTemporal3dBestBasis-' inFld '-' num2str(threshold) '-' datestr(now,'yyyymmddTHHMMSS') '/']; % Linux result folder  
@@ -38,7 +38,7 @@ debugMode = 0;
 
 FrameArray = zeros(RowFrames,ColFrames,NumFrames);
 for frame_index = 1:NumFrames
-    im_name                     = [imgPath 'hall_objects_',num2str(frame_index-1,'%04d'),'.jpg'];
+    im_name                     = [imgPath 'akiyo_',num2str(frame_index-1,'%04d'),'.jpg'];
     im                          = double(rgb2gray(imread(im_name)));
     FrameArray(:,:,frame_index) = im;
 end;
@@ -67,11 +67,10 @@ for frame_index = 1:NumFrames-PatchSize+1
                 % Input: CostValues,numLevels
                 % Output: basis and score
                 if BestBasisFlg == 1
-                    %% Cost Value Calculation function            
-                    [dwt_patch,CostValues] = CostValueCalculation(patch,numLevels); 
-%                     dwt_patch = dwt_patch(:);
-                    [Basis,DwtScore] = BestBasisSearch(CostValues,numLevels);
-                    SpatialDwtScoreArray((row_index+PatchSize-1)/PatchSize,(col_index+PatchSize-1)/PatchSize,frame_index) = DwtScore;
+                    %% Cost Value Calculation function
+                    % Best Basis Function with input: patch, numLevels; and
+                    % output: DwtScore;
+                    SpatialDwtScoreArray((row_index+PatchSize-1)/PatchSize,(col_index+PatchSize-1)/PatchSize,frame_index) = BestBasis3d(patch,numLevels);
                 else
                     dwt_cube = dwt533d(patch);
                     % dwt_patch
@@ -190,82 +189,6 @@ function [P,r,c] = cutImages(I,M)
             P(:,:,ir*c + ic + 1) = I(xl:1:xr,yl:1:yr);        
         end
     end
-end
-
-function [basis,score] = BestBasisSearch(costValues,numLevels)
-%% BestBasisSearch function is for finding the minimum cost value when data are transformed.
-% Input: costValues,numLevels
-% Output: basis, score
-    %% Bottom-up search for the best basis
-    b = 8; % b base of dwt in 3d
-    basis = [zeros((b^(numLevels-1)-1)/(b-1),1);ones(b^(numLevels-1),1)];
-    for j = numLevels-1:-1:1
-        for k = (b^(j-1)-1)/(b-1)+1:1:(b^j-1)/(b-1)
-            v1 = costValues(k);
-            v2 = sum(costValues(k*b-6:k*b+1));
-            if v1 <= v2
-                basis(k) = 1;
-            else
-                costValues(k) = v2;
-            end
-        end
-    end
-
-    %% Fill with 2's below the chosen basis
-    for k = 1:1:((b^(numLevels-1)-1)/(b-1))
-        if basis(k) == 1 || basis(k) == 2
-            basis(k*b-6:k*b+1) = 2;
-        end
-    end
-
-    basis = basis.*(basis == 1);            
-    costValues = costValues.*(basis == 1);
-    score = sum(costValues);
-end
-
-function [DwtPatch,CostValues] = CostValueCalculation(patch,numLevels,costFunc)
-%% Cost function
-% Input: numLevels: number of dividing levels
-%        patch: a PatchSizexPatchSize patch, i.e 4x4,8x8,16x16 patches.
-% Output: CostValues: cost value of patches with different levels
-    b = 8; % b base of dwt3d is 8
-    PatchSize = size(patch,1);
-    costValue_index = 0;
-    CostValues = zeros((b^numLevels-1)/(b-1),1);            
-    noPatchRows = size(patch,1);
-    noPatchCols = size(patch,2);
-    noPatchLays = size(patch,3);
-    for level_index = 0:1:(numLevels-1)               
-        %patch_size
-        subPatchSize = PatchSize / 2^level_index;        
-        for row_index = 1:subPatchSize:noPatchRows
-            for col_index = 1:subPatchSize:noPatchCols
-                for lay_index = 1:subPatchSize:noPatchLays
-                    subPatch = patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1),lay_index:(lay_index+subPatchSize-1));
-                    subPatch = dwt533d(subPatch);
-                    patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1),lay_index:(lay_index+subPatchSize-1)) = subPatch;
-                    % calculate shannon entropy for dct patch
-                    a        = subPatch(:);
-                    if sum(a ~= 0) > 0
-                        a        = a ./ norm(a);
-                        DwtScore = -sum(a(find(a)).^2.*log(a(find(a)).^2));
-                        costValue_index = costValue_index + 1;
-                        CostValues(costValue_index) = DwtScore;
-                    else
-                        costValue_index = costValue_index + 1;
-                        CostValues(costValue_index) = 0;
-                    end
-                    
-%                     if (level_index < numLevels-1) 
-%     %                     subPatch = subPatch./norm(subPatch);                
-%     %                     [cA,cH,cV,cD] = dwt2(subPatch,'db1');   % have to do dwt2 on two-dimensional 4x4 block                        
-%                         patch(row_index:(row_index+subPatchSize-1),col_index:(col_index+subPatchSize-1),lay_index:(lay_index+subPatchSize-1)) = dwt533d(subPatch);
-%                     end
-                end
-            end
-        end      
-    end
-    DwtPatch = patch;
 end
 
 function dct_cube = dct3(patch,PatchSize)
